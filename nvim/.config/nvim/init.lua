9e8c932a04328b78f2743040e6d24b66ba5e4839
Daniel Boj <dboj@uoc.edu>
2024-12-05 22:11:12 +0100

    Update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1103,6 +1103,20 @@ require("lazy").setup({
 		end,
 	},
 
+	-- Vim Tests
+	{
+		"vim-test/vim-test",
+		dependencies = {
+			"preservim/vimux",
+		},
+		vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>"),
+		vim.keymap.set("n", "<leader>tT", "<cmd>TestFile<CR>"),
+		vim.keymap.set("n", "<leader>ta", "<cmd>TestSuite<CR>"),
+		vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>"),
+		vim.keymap.set("n", "<leader>tg", "<cmd>TestVisit<CR>"),
+		vim.cmd("let test#strategy = 'vimux'"),
+	},
+
 	-- Themes
 	-- Bluloco
 	{
