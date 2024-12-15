cdb75a669cf774fdf85fd0ec65c8541381d286c7
Daniel Boj <dboj@uoc.edu>
2024-12-15 09:25:42 +0100

    Update

--- wp-changer/.config/hypr/wp-changer.sh
+++ wp-changer/.config/hypr/wp-changer.sh
@@ -3,5 +3,5 @@ while true; do
     wallpaper=$(find -L ~/Pictures/wp/cycle_wp -type f | shuf -n 1) 
     hyprctl hyprpaper preload $wallpaper
     hyprctl hyprpaper wallpaper ,$wallpaper 
-    sleep 21600 # 21600 segundos = 6 horas
+    sleep 10550
 done
